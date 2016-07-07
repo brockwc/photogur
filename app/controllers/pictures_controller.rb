9d55801e17be902db26b213562eb4a44cad5ab4a
zap-rowsdower <brock.whitbread@gmail.com>
2016-07-07 10:58:26 -0400

    remove double directory and initialize git in main project repo

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,50 @@
+class PicturesController < ApplicationController
+  
+  def index
+    @pictures = Picture.all
+    @most_recent_pictures = Picture.most_recent_five
+    @over_one_month_old = Picture.created_before(1.month.ago)
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = '931332a875488cebfb2eb8de6554bd6526cbd0735583b1f836e7b35c7fff9da8d3a66750722db023b598c47587bcb6be935529a3947d04fab4d6805ed11bd952'
